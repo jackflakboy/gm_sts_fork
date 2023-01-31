1e130154c0a7d5dfd7d40b82c39eba7395eea40a
Bobbyperson12 <bobbyperson12345@gmail.com>
2023-01-31 14:53:56 -0500

    more code cleanup. mostly formatting

--- gamemodes/sts/gamemode/custommenu.lua
+++ gamemodes/sts/gamemode/custommenu.lua
@@ -1,104 +1,96 @@
-
 local Menu
 
-net.Receive("FMenu",function()
-	if (Menu == nil) then
-		Menu = vgui.Create("DFrame")
-		Menu:SetSize(750,500)
-		Menu:SetPos(ScrW() / 2-325,ScrH() / 2-250)
-		Menu:SetTitle("Gamemode Menu")
-		Menu:SetDraggable(false)
-		Menu:ShowCloseButton(true)
-		Menu:SetDeleteOnClose(false)
+net.Receive("FMenu", function()
+    if Menu == nil then
+        Menu = vgui.Create("DFrame")
+        Menu:SetSize(750, 500)
+        Menu:SetPos(ScrW() / 2 - 325, ScrH() / 2 - 250)
+        Menu:SetTitle("Gamemode Menu")
+        Menu:SetDraggable(false)
+        Menu:ShowCloseButton(true)
+        Menu:SetDeleteOnClose(false)
 
-		Menu.Paint = function()
-			surface.SetDrawColor(60,60,60,255)
-			surface.DrawRect(0,0,Menu:GetWide(),Menu:GetTall())
+        Menu.Paint = function()
+            surface.SetDrawColor(60, 60, 60, 255)
+            surface.DrawRect(0, 0, Menu:GetWide(), Menu:GetTall())
+            surface.SetDrawColor(40, 40, 40, 255)
+            surface.DrawRect(0, 24, Menu:GetWide(), 1)
+        end
+    end
 
-			surface.SetDrawColor(40,40,40,255)
-			surface.DrawRect(0,24,Menu:GetWide(),1)
+    addButtons(Menu)
 
-		end
-	end
-
-	addButtons(Menu)
-	if (net.ReadBit() == 0) then
-		Menu:Hide()
-		gui.EnableScreenClicker(false)
-	else
-		Menu:Show()
-		gui.EnableScreenClicker(true)
-	end
+    if net.ReadBit() == 0 then
+        Menu:Hide()
+        gui.EnableScreenClicker(false)
+    else
+        Menu:Show()
+        gui.EnableScreenClicker(true)
+    end
 end)
 
-
-
 function addButtons(Menu)
-	local blueButton = vgui.Create("DButton")
-	blueButton:SetParent(Menu)
-	blueButton:SetText("")
-	blueButton:SetSize(Menu:GetWide(),50)
-	blueButton:SetPos(0,25)
-	blueButton.Paint = function()
-		surface.SetDrawColor(30,30,255,255)
-		surface.DrawRect(0,0,blueButton:GetWide(),blueButton:GetTall())
+    local blueButton = vgui.Create("DButton")
+    blueButton:SetParent(Menu)
+    blueButton:SetText("")
+    blueButton:SetSize(Menu:GetWide(), 50)
+    blueButton:SetPos(0, 25)
 
+    blueButton.Paint = function()
+        surface.SetDrawColor(30, 30, 255, 255)
+        surface.DrawRect(0, 0, blueButton:GetWide(), blueButton:GetTall())
+        draw.DrawText("Blue", "DermaDefaultBold", blueButton:GetWide() / 2, 17, Color(255, 255, 255, 255), 1)
+    end
 
-		draw.DrawText("Blue", "DermaDefaultBold",blueButton:GetWide() / 2,17,Color(255,255,255,255),1)
+    blueButton.DoClick = function(blueButton)
+        LocalPlayer():ConCommand("set_team " .. 1)
+    end
 
-	end
-	blueButton.DoClick = function(blueButton)
-		LocalPlayer():ConCommand("set_team " .. 1)
-	end
+    local redButton = vgui.Create("DButton")
+    redButton:SetParent(Menu)
+    redButton:SetText("")
+    redButton:SetSize(Menu:GetWide(), 50)
+    redButton:SetPos(0, 100)
 
+    redButton.Paint = function()
+        surface.SetDrawColor(255, 30, 30, 255)
+        surface.DrawRect(0, 0, redButton:GetWide(), redButton:GetTall())
+        draw.DrawText("Red", "DermaDefaultBold", redButton:GetWide() / 2, 17, Color(255, 255, 255, 255), 1)
+    end
 
-	local redButton = vgui.Create("DButton")
-	redButton:SetParent(Menu)
-	redButton:SetText("")
-	redButton:SetSize(Menu:GetWide(),50)
-	redButton:SetPos(0,100)
-	redButton.Paint = function()
-		surface.SetDrawColor(255,30,30,255)
-		surface.DrawRect(0,0,redButton:GetWide(),redButton:GetTall())
+    redButton.DoClick = function(redButton)
+        LocalPlayer():ConCommand("set_team " .. 2)
+    end
 
-		draw.DrawText("Red", "DermaDefaultBold",redButton:GetWide() / 2,17,Color(255,255,255,255),1)
-	end
-	redButton.DoClick = function(redButton)
-		LocalPlayer():ConCommand("set_team " .. 2)
-	end
+    local greenButton = vgui.Create("DButton")
+    greenButton:SetParent(Menu)
+    greenButton:SetText("")
+    greenButton:SetSize(Menu:GetWide(), 50)
+    greenButton:SetPos(0, 175)
 
+    greenButton.Paint = function()
+        surface.SetDrawColor(0, 255, 0, 255)
+        surface.DrawRect(0, 0, greenButton:GetWide(), greenButton:GetTall())
+        draw.DrawText("Green", "DermaDefaultBold", greenButton:GetWide() / 2, 17, Color(255, 255, 255, 255), 1)
+    end
 
-	local greenButton = vgui.Create("DButton")
-	greenButton:SetParent(Menu)
-	greenButton:SetText("")
-	greenButton:SetSize(Menu:GetWide(),50)
-	greenButton:SetPos(0,175)
-	greenButton.Paint = function()
-		surface.SetDrawColor(0,255,0,255)
-		surface.DrawRect(0,0,greenButton:GetWide(),greenButton:GetTall())
+    greenButton.DoClick = function(greenButton)
+        LocalPlayer():ConCommand("set_team " .. 3)
+    end
 
+    local yellowButton = vgui.Create("DButton")
+    yellowButton:SetParent(Menu)
+    yellowButton:SetText("")
+    yellowButton:SetSize(Menu:GetWide(), 50)
+    yellowButton:SetPos(0, 250)
 
-		draw.DrawText("Green", "DermaDefaultBold",greenButton:GetWide() / 2,17,Color(255,255,255,255),1)
+    yellowButton.Paint = function()
+        surface.SetDrawColor(255, 255, 0, 255)
+        surface.DrawRect(0, 0, yellowButton:GetWide(), yellowButton:GetTall())
+        draw.DrawText("Yellow", "DermaDefaultBold", yellowButton:GetWide() / 2, 17, Color(0, 0, 0, 255), 1)
+    end
 
-	end
-	greenButton.DoClick = function(greenButton)
-		LocalPlayer():ConCommand("set_team " .. 3)
-	end
-
-	local yellowButton = vgui.Create("DButton")
-	yellowButton:SetParent(Menu)
-	yellowButton:SetText("")
-	yellowButton:SetSize(Menu:GetWide(),50)
-	yellowButton:SetPos(0,250)
-	yellowButton.Paint = function()
-		surface.SetDrawColor(255,255,0,255)
-		surface.DrawRect(0,0,yellowButton:GetWide(),yellowButton:GetTall())
-
-
-		draw.DrawText("Yellow", "DermaDefaultBold",yellowButton:GetWide() / 2,17,Color(0,0,0,255),1)
-
-	end
-	yellowButton.DoClick = function(yellowButton)
-		LocalPlayer():ConCommand("set_team " .. 4)
-	end
+    yellowButton.DoClick = function(yellowButton)
+        LocalPlayer():ConCommand("set_team " .. 4)
+    end
 end
--- gamemodes/sts/gamemode/teamsetup.lua
+++ gamemodes/sts/gamemode/teamsetup.lua
@@ -2,32 +2,33 @@ local ply = FindMetaTable("Player")
 local teams = {}
 
 teams[1] = {
-	name = "Blue", 
-	color = Vector(0.2,0.2,1.0)}
-	
+    name = "Blue",
+    color = Vector(0.2, 0.2, 1.0)
+}
+
 teams[2] = {
-	name = "Red", 
-	color = Vector(1.0,0,0)}
-	
+    name = "Red",
+    color = Vector(1.0, 0, 0)
+}
 
 teams[3] = {
-	name = "Green", 
-	color = Vector(0.0,1.0,0.0)}
-	
+    name = "Green",
+    color = Vector(0.0, 1.0, 0.0)
+}
+
 teams[4] = {
-	name = "Yellow", 
-	color = Vector(1.0,1.0,0.0)}
+    name = "Yellow",
+    color = Vector(1.0, 1.0, 0.0)
+}
 
 teams[0] = {
-	name = "Empty", 
-	color = Vector(1.0,1.0,1.0)}
-		
+    name = "Empty",
+    color = Vector(1.0, 1.0, 1.0)
+}
+
 function ply:SetupTeam(n)
-	if not teams[n] then return end
-	self:SetTeam(n)
-	self:SetPlayerColor(teams[n].color)
-	self:SetModel("models/player/group03m/male_07.mdl")
-	
+    if not teams[n] then return end
+    self:SetTeam(n)
+    self:SetPlayerColor(teams[n].color)
+    self:SetModel("models/player/group03m/male_07.mdl")
 end
-
-
